257a61da91e6ec7328eac756b89a0eecf516486a
owenmoogk <owenmoogk@gmail.com>
2021-04-27 19:13:07 -0400

    cleaned up some loose ends

--- src/components/projects/ProjectPage.js
+++ src/components/projects/ProjectPage.js
@@ -1,29 +1,26 @@
 import React from 'react';
+import {useParams} from 'react-router-dom'
+import 'css/projects/project-pages.css'
 
 export default function ProjectIcon(props) {
 
 	// the function called when loading page
 	function loadProjectPage(xmlPage) {
-		var x;
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				x = projectPage(this);
-				return x
+			if (this.readyState === 4 && this.status === 200) {
+				projectPage(this);
 			}
 		};
 		xmlhttp.open("GET", xmlPage, true);
 		xmlhttp.send();
-		console.log(x)
-		return(x)
 	}
 
 	// actual loading process
 	function projectPage(xml) {
 
 		// xml doxument
-		var xmlDoc;
-		xmlDoc = xml.responseXML;
+		var xmlDoc = xml.responseXML;
 
 		// entire html of page
 		var txt = "";
@@ -31,7 +28,7 @@ export default function ProjectIcon(props) {
 		// load header of page
 		var pageTitle = xmlDoc.getElementsByTagName('page-title')[0].innerHTML
 		var date = xmlDoc.getElementsByTagName('date')[0].innerHTML
-		txt += '<div className="title"><p style="line-height: 70px;">'+pageTitle+'</p></div><div className="date"><p className="pdate">'+date+'</p></div>';
+		txt += '<div className="page-title"><p style="line-height: 70px;">'+pageTitle+'</p></div><div className="date"><p className="pdate">'+date+'</p></div>';
 
 
 		// load main part of the page
@@ -50,26 +47,26 @@ export default function ProjectIcon(props) {
 				var elementData = currBlock[blockElementNum].innerHTML // contains the inner data for the element
 
 				// appending text to var 'txt' depending on tag name
-				if (elementType == 'title'){
+				if (elementType === 'title'){
 					txt += '<div className="text"><h1>'+elementData+'</h1>'
 				}
-				else if (elementType == 'text'){
+				else if (elementType === 'text'){
 					
 					// add in breaks for ~~
 					var exitLoop = false
-					while (exitLoop == false){
+					while (exitLoop === false){
 						elementData = elementData.replace('~~','<br>')
 						var i = elementData.search('~~')
-						if (i == -1){
+						if (i === -1){
 							exitLoop = true
 						}
 					}
 					txt += '<p>'+elementData+'</p></div>'
 				}
-				else if (elementType == 'image'){
+				else if (elementType === 'image'){
 					txt += '<div className="img"><img src="'+elementData+'" className="img"></div>'
 				}
-				else if (elementType == 'render'){
+				else if (elementType === 'render'){
 					txt += '<div className="render"><img src="'+elementData+'" className="img"></div>'
 				}
 				else{
@@ -80,12 +77,16 @@ export default function ProjectIcon(props) {
 			txt += '</div>' // close out block div
 
 		}
-		return(txt)
+		document.getElementsByClassName("body")[0].innerHTML = txt;
 	}
 
+	// passed in from the router
+	var {id} = useParams()
+	var xmlFileLink = process.env.PUBLIC_URL + '/assets/projects/'+id+".xml"
+	
 	return (
 		<div className="body">
-			{console.log(loadProjectPage('/assets/projects/2702-2019.xml'))}
+			{loadProjectPage(xmlFileLink)}
 		</div>
 	);
 }
