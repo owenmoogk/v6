c815bd4b844e5114260d672dfd53fb03a6cdd3d8
owenmoogk <owenmoogk@gmail.com>
2021-04-30 15:41:26 -0400

    moves the background file to src, is now called when background is rendered instead. now I don;t have to rely on it being in index.html

--- src/components/homepage/Background.js
+++ src/components/homepage/Background.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {loadBackground} from './backgroundHelper.js'
 
 export default function Background(props) {
+
+	useEffect(() => {
+		loadBackground();
+	}, []);
 
 	return (
 		<canvas id='c' style={{zIndex: -1}}></canvas>
